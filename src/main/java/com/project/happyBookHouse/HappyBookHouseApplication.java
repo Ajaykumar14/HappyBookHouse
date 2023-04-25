1a11f8b5f1380d2d225e31ca9708b00d04eeee2f
Ajay Kumar <ajayTamilarasan@gmail.com>
2023-04-25 22:46:38 +0530

    Initail Commit

---
+++ src/main/java/com/project/happyBookHouse/HappyBookHouseApplication.java
@@ -0,0 +1,13 @@
+package com.project.happyBookHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyBookHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyBookHouseApplication.class, args);
+	}
+
+}
